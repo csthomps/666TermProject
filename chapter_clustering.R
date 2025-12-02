23136eca19886b9dcb322931f6e87e3112cbe3fd
Camilla McKinnon <camilla@Queens-Air-3.app.byu.edu>
2025-12-02 14:27:44 -0700

    updating with cluster interpretation code

--- chapter_clustering.R
+++ chapter_clustering.R
@@ -1,7 +1,7 @@
 ############ Stat 666 Term Project  ##############
 ############ Code for Clustering    ##############
 
-### SET UP
+##### SET UP
 library(cluster)
 library(ggplot2)
 library(tidyr)
@@ -9,14 +9,15 @@ library(dplyr)
 
 # setwd("termproj")
 chaps <- read.csv("tfidf_features_100.csv", header=TRUE)
-chaps <-chaps[,-1]
+text_names <- chaps[, 1]  # first column = text names (chapter names)
+chaps <-chaps[,-1] # remove the chapter names (so "1 Nephi", etc., isn't being used in clustering)
 dim(chaps)
-# no need to standardize b/c TF–IDF, TFIDF features on same numerical scale
+# no need to standardize b/c TFIDF features on same numerical scale
 
 # get distance
 feat_dist <- dist(data.matrix(chaps))
 
-##### CLUSTER COMPUTATIONS
+##### AGGLOMERATIVE METHOD CLUSTER COMPUTATIONS
 
 par(mfrow=c(1,3))
 
@@ -32,41 +33,40 @@ plot(avglink,labels=labels(chaps)[[1]])
 wardlink <- hclust(feat_dist,method='ward.D2')
 plot(wardlink,labels=labels(chaps)[[1]])
 
-par(mfrow=c(1,1))
-
+# Visualizing Ward's linkage with clusters
 # Plot dendrogram with colored cluster borders
-k <- 7  # choose some number
+par(mfrow=c(1,1))
+k <- 7  # choose some number by looking at it, I picked 7
 groups <- cutree(wardlink, k = k)
 plot(wardlink, labels = labels(chaps)[[1]])
 rect.hclust(wardlink, k = k, border = 2:8)
 
+# Calculating Balance for Ward Method K=7
 tab <- table(groups)
 tab
 tab / sum(tab) # percentage of chapters in each cluster
 sd(as.numeric(tab)) # standard deviation of cluster sizes
 
-## K MEANS
-k <- 11  # choose number of clusters
+##### K MEANS CLUSTERING METHOD
+
+k <- 9  # choose num clusters, this is just a starting point, will check others later
 k_means <- kmeans(chaps, centers = k, nstart = 25)  # nstart = 25 tries different random starts
 groups <- k_means$cluster
-table(groups) # inspect cluster sizes
+table(groups) # cluster sizes / how many chapters sorted into each cluster
 sd(as.numeric(table(groups))) # SD of cluster sizes
 
-### trying to find the best cluster size --------------------
+##### EXPLORE THE BEST CLUSTER METHOD AND K SIZE --------------------
 
-# clustering methods
+# agglomerative clustering methods
 methods <- list(
   complete = complink,
   average  = avglink,
-  ward     = wardlink#,
-  # kmean    = k_means
+  ward     = wardlink
 )
 # k clusters to try
-ks <- 5:12
+ks <- 5:12 # this range was selected after looking at the wards dendrogram
 
-### compute SD of cluster sizes ####
-# this is code to print them in a matrix
-# # storage matrix
+# this is code to print them SD of cluster sizes in a matrix
 # sd_mat <- matrix(NA_real_,
 #                  nrow = length(methods),
 #                  ncol = length(ks),
@@ -82,9 +82,7 @@ ks <- 5:12
 # }
 # sd_mat
 
-### visualize results
-
-# Compute SD of cluster sizes
+# Compute SD of cluster sizes (balance)
 sd_list <- data.frame()
 
 for(m in names(methods)){
@@ -133,15 +131,42 @@ for (m in names(methods)) {
 }
 km_sil_list
 
+# Leaning towards k-means as the best method, by balance
+# and by silhouette score. Although, all the silhouette scores are pretty bad.
 
-##################################
 
-# I'm kinda leaning towards k-means as the best method, by balance
-# and by silhouette score? so a cluster size of perhaps 8????
-# 12 feels like two many, 5 wouldn't be terrible, I feel like it might
-# depend on PCA/ discriminant analysis
+##### EXPLORE AND INTERPRET RESULTS --------------------------------------
 
-# My next steps are to see if I can print out what chapters go in what 
-# clusters, to see if I can ascribe any meaning to this
+k <- 5  # choose number of clusters
+k_means <- kmeans(chaps, centers = k, nstart = 25)
+clusters <- k_means$cluster
 
-# then do the write up and be done woohoo 
+tfidf <- as.matrix(chaps)
+cluster_means <- aggregate(tfidf, by = list(cluster = clusters), FUN = mean)
+rownames(cluster_means) <- paste0("Cluster_", cluster_means$cluster)
+cluster_means$cluster <- NULL
+cluster_means <- as.matrix(cluster_means)
+# Now cluster_means is an 8 × p matrix:
+# rows = clusters
+# columns = words
+# values = mean TF–IDF per cluster
+
+# extract top words from each cluster
+top_words <- list()
+for (i in 1:5) {
+  # sort words by decreasing mean tf-idf
+  sorted <- sort(cluster_means[i, ], decreasing = TRUE)
+  # take the top 10 most characteristic words
+  top_words[[paste0("Cluster_", i)]] <- head(sorted, 10)
+}
+top_words
+
+# view which chapters went with which clusters
+cluster_members <- split(text_names, clusters)
+cluster_members
+
+### Potential LDA code
+library(MASS)
+lda_fit <- lda(clusters ~ ., data = data.frame(clusters = clusters, tfidf))
+lda_fit
+
